7cbd9b5527e949ee4d18181b40199b6ddafaaa30
mohamed-wahbi <wahbisj@gmail.com>
2026-05-22 08:55:41 +0100

    .1

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css"
 import ClientLayout from "@/components/ui/ClientLayout"
 
 export const metadata: Metadata = {
-  metadataBase: new URL("http://localhost:3000"),
+  metadataBase: new URL("https://www.nexolia-consulting.com"),
   title: {
     default: "NEXOLIA | Agence IT Tunisie & ESN Partenaire Tech en France",
     template: "%s | NEXOLIA",
